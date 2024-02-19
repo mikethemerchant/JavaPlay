0d7ff75e9c1b04a872e5aca019b3b1f403385714
Michael <mikethemerchant@gmail.com>
2024-02-19 11:43:39 -0500

    at switch lunch break

---
+++ src/ifThenElseSwitch.java
@@ -0,0 +1,15 @@
+public class ifThenElseSwitch {
+    public static void main(String arg[]) {
+        int i = 2;
+        int x = 2;
+
+        if(i < x) {
+            System.out.println("TRUE");
+        } else if (i == x) {
+            System.out.println("EQUAL");
+        } else {
+            System.out.println("FALSE");
+        }
+        
+    }
+}
