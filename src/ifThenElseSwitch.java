3de9bed3192fa1fb29ab87bbc05eb1bcc82a94d1
Michael <mikethemerchant@gmail.com>
2024-02-19 14:08:28 -0500

    switch

--- src/ifThenElseSwitch.java
+++ src/ifThenElseSwitch.java
@@ -1,14 +1,31 @@
 public class ifThenElseSwitch {
     public static void main(String arg[]) {
         int i = 2;
-        int x = 2;
-
+        int x = 1;
+        int j = 3;
+        
         if(i < x) {
             System.out.println("TRUE");
         } else if (i == x) {
             System.out.println("EQUAL");
         } else {
             System.out.println("FALSE");
+        }
+
+        String r = (i > x) ? "YES" : "NO";
+        System.out.println(r);
+
+        switch(j) {
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            case 3:
+                System.out.println("3");
+                break;
+            default:
         }
         
     }
